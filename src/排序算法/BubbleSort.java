9fea19975886f048f27c311fe503815e7bcfb2c5
liu.gongding@oyohotels.cn <https://github.com/liugongding/testGit.git>
2019-09-25 18:05:28 +0800

    :sparkles:冒泡排序

---
+++ src/排序算法/BubbleSort.java
@@ -0,0 +1,36 @@
+package 排序算法;
+
+import java.util.Arrays;
+
+/**
+ * @Description 冒泡排序
+ * @Author liugongding
+ * @Date 2019-09-25
+ */
+public class BubbleSort {
+
+    /**
+     *  5, 7, 2, 9, 4, 1, 0, 5, 7
+     *  总共比较arr.length-1轮，第一个数自己不和自己比较，比较完之后，最大的数就放到了最后面
+     *
+     * @param arr
+     */
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{5, 7, 2, 9, 4, 1, 0, 5, 7};
+        System.out.println(Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
